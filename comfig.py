1c67370bde6af44a8de7c70caaa2ef43d8e8153c
Iroennys <100889237+Iroeli@users.noreply.github.com>
2022-06-01 21:30:18 -0400

    Update comfig.py

--- comfig.py
+++ comfig.py
@@ -3,8 +3,8 @@ API_ID = '14681595'
 API_HASH = 'a86730aab5c59953c424abb4396d32d5'
 ADMIN_ID = [1759969205]
 
-MOODLE_URL = 'http://www.aula.scu.sld.cu/'
-MOODLE_USER = 'darian.borges@estudiantes.fbio.uh.cu'
-MOODLE_PASSWORD = 'darian1995'
+MOODLE_URL = 'https://eduvirtual.uho.edu.cu/'
+MOODLE_USER = 'lilianats@uho.edu.cu'
+MOODLE_PASSWORD = '1234567m'
 PARTES_MB = 100
 
